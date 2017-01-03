f53f9ef437c1b4bc1e6562cf32a62db48d181a5e
James Stine <Leon.Blade@gmail.com>
2017-01-02 23:29:36 -0500

    First commit

---
+++ CasksAnywhere/CasksAnywhere.cs
@@ -0,0 +1,45 @@
+﻿using StardewValley;
+using StardewValley.Objects;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI.Inheritance;
+
+namespace CasksAnywhere
+{
+	public class CasksAnywhere : Mod
+	{
+		public override void Entry(IModHelper helper)
+		{
+			// add event listener for when inventory changes
+			PlayerEvents.InventoryChanged += PlayerEvents_InventoryChanged;
+		}
+
+		void PlayerEvents_InventoryChanged(object sender, EventArgsInventoryChanged e)
+		{
+			// loop through the changes that were removed
+			foreach (var change in e.Removed)
+				if (change.ChangeType == ChangeType.Removed && change.Item.Name == "Cask")
+					hijackCask();
+		}
+
+		private void hijackCask()
+		{
+			// get the tile location
+			var tileLocation = new Vector2(
+				(Game1.viewport.X + Game1.getOldMouseX()) / Game1.tileSize,
+				(Game1.viewport.Y + Game1.getOldMouseY()) / Game1.tileSize
+			);
+
+			// if our location is valid and we have an object in this location
+			if (Game1.currentLocation != null && Game1.currentLocation.objects.ContainsKey(tileLocation))
+			{
+				// the item in question
+				Item item = Game1.currentLocation.objects[tileLocation];
+				// if this is a cask then hijack it!
+				if (item != null && item is Cask)
+					Game1.currentLocation.objects[tileLocation] = new HijackCask(item as Cask, tileLocation);
+			}
+		}
+	}
+}
---
+++ CasksAnywhere/HijackCask.cs
@@ -0,0 +1,101 @@
+﻿using StardewValley;
+using StardewValley.Objects;
+using Microsoft.Xna.Framework;
+
+namespace CasksAnywhere
+{
+	public class HijackCask : Cask
+	{
+		public HijackCask(Cask b, Vector2 v) : base(v)
+		{
+			this.heldObject = b.heldObject;
+			this.agingRate = b.agingRate;
+			this.daysToMature = b.daysToMature;
+			this.minutesUntilReady = b.minutesUntilReady;
+		}
+
+		public override bool performObjectDropInAction(StardewValley.Object dropIn, bool probe, Farmer who)
+		{
+			if (dropIn != null && dropIn.bigCraftable)
+				return false;
+
+			if (this.heldObject != null)
+				return false;
+
+			if (this.quality >= 4)
+				return false;
+			
+			bool flag = false;
+			float num = 1f;
+			int psi = dropIn.parentSheetIndex;
+
+			switch (psi)
+			{
+				case 346:
+					flag = true;
+					num = 2f;
+					break;
+				case 348:
+					flag = true;
+					num = 1f;
+					break;
+				default:
+					switch (psi - 424)
+					{
+						case 0:
+							flag = true;
+							num = 4f;
+							break;
+						case 2:
+							flag = true;
+							num = 4f;
+							break;
+						default:
+							if (psi != 303)
+							{
+								if (psi == 459)
+								{
+									flag = true;
+									num = 2f;
+									break;
+								}
+								break;
+							}
+							flag = true;
+							num = 1.66f;
+							break;
+					}
+					break;
+			}
+
+			if (!flag)
+				return false;
+			
+			this.heldObject = dropIn.getOne() as StardewValley.Object;
+
+			if (!probe)
+			{
+				this.agingRate = num;
+				this.daysToMature = 56f;
+				this.minutesUntilReady = 999999;
+				if (this.heldObject.quality == 1)
+					this.daysToMature = 42f;
+				else if (this.heldObject.quality == 2)
+					this.daysToMature = 28f;
+				else if (this.heldObject.quality == 4)
+				{
+					this.daysToMature = 0.0f;
+					this.minutesUntilReady = 1;
+				}
+				Game1.playSound("Ship");
+				Game1.playSound("bubbles");
+				who.currentLocation.temporarySprites.Add(new TemporaryAnimatedSprite(Game1.animations, new Rectangle(256, 1856, 64, 128), 80f, 6, 999999, this.tileLocation * (float)Game1.tileSize + new Vector2(0.0f, (float)(-Game1.tileSize * 2)), false, false, (float)(((double)this.tileLocation.Y + 1.0) * (double)Game1.tileSize / 10000.0 + 9.99999974737875E-05), 0.0f, Color.Yellow * 0.75f, 1f, 0.0f, 0.0f, 0.0f, false)
+				{
+					alphaFade = 0.005f
+				});
+			}
+			return true;
+		}
+	}
+
+}
